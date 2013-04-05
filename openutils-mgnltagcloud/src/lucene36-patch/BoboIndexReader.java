ac358fd5b0e4380940cb9754a781331fe54187ae
fgiust <fgiust@8cde5574-a10d-0410-8942-c3fc08a91110>
2013-04-05 20:53:53 +0000

    license header (to avoid build failure)
    git-svn-id: svn://svn.code.sf.net/p/openutils/code/magnoliamodules/trunk@4218 8cde5574-a10d-0410-8942-c3fc08a91110

--- openutils-mgnltagcloud/src/lucene36-patch/BoboIndexReader.java
+++ openutils-mgnltagcloud/src/lucene36-patch/BoboIndexReader.java
@@ -1,3 +1,22 @@
+/**
+ *
+ * Tagcloud module for Magnolia CMS (http://www.openmindlab.com/lab/products/mgnltagcloud.html)
+ * Copyright(C) 2010-2013, Openmind S.r.l. http://www.openmindonline.it
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 // lucene 3.6 compatibility patch for bobo-browse 3.2.0, see https://github.com/senseidb/bobo/commit/e78d91ef8a00965d8d55c819f967cac172c23d17
 /**
  * Bobo Browse Engine - High performance faceted/parametric search implementation 
